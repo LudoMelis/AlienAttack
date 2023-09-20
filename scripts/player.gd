79669ac360c99b06cb559b7331761ba5e5312bab
Ludo Melis <ludomelis06@gmail.com>
2023-09-20 15:48:32 +0200

    Clamped player position within screen range

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
 var force = 500
+var _screenSize
+
+func _ready():
+	pass
 
 func _physics_process(_delta):
 	velocity = Vector2(0, 0)
+	_screenSize = get_viewport_rect().size
 	if Input.is_anything_pressed():
-		print("x : " + str(Input.get_axis("Left", "Right")) + " - y : " + str(Input.get_axis("Up", "Down")))
 		velocity = Vector2(Input.get_axis("Left","Right") * force, Input.get_axis("Up", "Down") * force)
 	move_and_slide()
+	
+	global_position = global_position.clamp(Vector2(0, 0), _screenSize)
