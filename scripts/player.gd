19ab58f1bd32949e5b2553b9c1199c8faf86bfc4
Ludo Melis <ludomelis06@gmail.com>
2023-09-20 11:23:01 +0200

    Implements player movement & analog support

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,2 +1,10 @@
 extends CharacterBody2D
 
+var force = 500
+
+func _physics_process(_delta):
+	velocity = Vector2(0, 0)
+	if Input.is_anything_pressed():
+		print("x : " + str(Input.get_axis("Left", "Right")) + " - y : " + str(Input.get_axis("Up", "Down")))
+		velocity = Vector2(Input.get_axis("Left","Right") * force, Input.get_axis("Up", "Down") * force)
+	move_and_slide()
