b67ea2d53e98243dc0f05d11600ce756a42cea37
Ludo Melis <ludomelis06@gmail.com>
2023-09-25 15:38:25 +0200

    Delete rockets on screen exit

--- scenes/rocket.gd
+++ scenes/rocket.gd
@@ -2,5 +2,15 @@ extends Area2D
 
 @export var speed = 500
 
+#@onready var visibleNotifier = $VisibleNotifier
+
+func _ready():
+	pass
+	#visibleNotifier.connect("screen_exited", _onScreenExited)
+
 func _physics_process(delta):
 	global_position.x += speed*delta
+
+
+func _on_visible_notifier_screen_exited():
+	queue_free()
